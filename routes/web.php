c12aedd58a264b7e65c2bb0fc4e7d4a092faf4d8
jorge <jorgelllreynaldez@gmail.com>
2020-08-21 17:53:25 -0400

    calculadoraSimple

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//calculadora 
+//suma
+
+Route::get('calculadora', 'calController@calculadora');
+
+Route::post('solucioncal', ['as' => 'calculadora.solucioncal', 'uses' => 'calController@solucioncal']);
+
+//resta
+Route::get('calculadora', 'restaController@calculadora');
+
+Route::post('resta', ['as' => 'calculadora.resta', 'uses' => 'restaController@resta']);
+
+//division
+Route::get('calculadora', 'divisionController@calculadora');
+
+Route::post('division', ['as' => 'calculadora.division', 'uses' => 'divisionController@division']);
+ 
+//multiplicacion
+
+Route::get('calculadora', 'multiController@calculadora');
+
+Route::post('multi', ['as' => 'calculadora.multi', 'uses' => 'multiController@multi']);
+
+//potencia
+
+Route::get('calculadora', 'potController@calculadora');
+
+Route::post('pot', ['as' => 'calculadora.pot', 'uses' => 'potController@pot']);
+
+
